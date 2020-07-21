81501fc556b46b93883bd88b3b0833ca088dd41b
Roy  Flores <royfloresnyc@gmail.com>
2020-07-21 21:09:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,12 @@ require 'pry'
 
 def load_library(library)
   # code goes here
-  YAML.load_file(library)
+  yml_hash = YAML.load_file(library)
+  yml_hash.each do |meaning, array_of_emoticons|
+    yml_hash[meaning][:english] = array_of_emoticons.first
+    yml_hash[meaning][:japanese] = array_of_emoticons.last
+  end  
+  yml_hash
 end
 
 def get_japanese_emoticon
